76b0728c38cc0c0133fc27149c7f1439fb5d90e1
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-10-11 19:20:30 +0200

    Create constructor, getters and setters

--- src/main/java/com/example/members/model/NewUserAccount.java
+++ src/main/java/com/example/members/model/NewUserAccount.java
@@ -1,8 +1,41 @@
 package com.example.members.model;
 
+import java.util.ArrayList;
+
 public class NewUserAccount {
-    private String fName;
-    private String lName;
     private String username;
     private String password;
+
+    private final ArrayList<String> listUserAccounts = new ArrayList();
+
+    public NewUserAccount(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public static NewUserAccount createLoginAccount(String username, String password) {
+        return new NewUserAccount(username, password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public ArrayList<String> getListUserAccounts() {
+        return listUserAccounts;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
+
