5fa6fcf19b780581735bbcc902ff6ea08a52a540
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-11-01 20:49:31 +0100

    Add a todo comment

--- src/main/java/com/example/members/controller/UserInterface.java
+++ src/main/java/com/example/members/controller/UserInterface.java
@@ -132,6 +132,8 @@ public class UserInterface {
         }
     }
 
+    //TODO test to add a todo comment
+
     public void removeMemberFromDatabase() {
         System.out.println("Enter position to remove: ");
         int positionToRemove = scanner.nextInt();
