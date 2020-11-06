af61dfb294f09e2ae93f87df1fd67b80644dd931
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-11-06 20:42:52 +0100

    Create constructor

--- src/main/java/com/example/members/controller/Login.java
+++ src/main/java/com/example/members/controller/Login.java
@@ -1,4 +1,12 @@
 package com.example.members.controller;
 
 public class Login {
+
+    private String username;
+    private String password;
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
