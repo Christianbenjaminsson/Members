7d359fee260e6958c91f72b91f133f7a0634b38b
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-11-08 10:20:26 +0100

    Set up page

--- src/main/java/com/example/members/controller/LoginUserInterface.java
+++ src/main/java/com/example/members/controller/LoginUserInterface.java
@@ -1,12 +1,34 @@
 package com.example.members.controller;
 
+import com.example.members.model.LoginMemberDetails;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LoginUserInterface {
 
     private String username;
     private String password;
 
+    boolean quit = false;
+    Scanner scanner = new Scanner(System.in);
+    private final ArrayList<LoginMemberDetails> saveUserLogin = new ArrayList<>();
+
     public LoginUserInterface(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public void start() {
+        System.out.print("Enter username: ");
+        String loginUsername = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String loginPassword = scanner.nextLine();
+        LoginMemberDetails useLoginMemberDetails = new LoginMemberDetails("username", "password");
+        if (loginUsername.equals(useLoginMemberDetails.getSavedUserName()) &&
+        loginPassword.equals(useLoginMemberDetails.getSaveUserPassword())) {
+            UserInterface goToUserInterface = new UserInterface();
+            goToUserInterface.start();
+        }
     }
 }
