18a1127e674626ece2226882cb6068148d581f7f
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-11-08 10:21:13 +0100

    Set up page

--- src/main/java/com/example/members/model/LoginMemberDetails.java
+++ src/main/java/com/example/members/model/LoginMemberDetails.java
@@ -1,4 +1,27 @@
 package com.example.members.model;
 
 public class LoginMemberDetails {
+    private String savedUserName;
+    private String saveUserPassword;
+
+    public LoginMemberDetails(String savedUserName, String saveUserPassword) {
+        this.savedUserName = savedUserName;
+        this.saveUserPassword = saveUserPassword;
+    }
+
+    public String getSavedUserName() {
+        return savedUserName;
+    }
+
+    public String getSaveUserPassword() {
+        return saveUserPassword;
+    }
+
+    public void setSavedUserName(String savedUserName) {
+        this.savedUserName = savedUserName;
+    }
+
+    public void setSaveUserPassword(String saveUserPassword) {
+        this.saveUserPassword = saveUserPassword;
+    }
 }
